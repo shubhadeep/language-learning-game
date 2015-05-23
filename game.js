ff89d9d96e2f69c05c717abce01fdab252e6c8cb
Shubhadeep Dhar <shubhadeep@gmail.com>
2015-05-24 00:11:35 +0530

    jQueryfied

--- game.js
+++ game.js
@@ -1,45 +1,71 @@
-;(function (window) {
+;(function (window, $) {
   "use strict";
 
-  var gameData = {
-    challenges: [
-      { question: "question1", answer: "answer"},
-      { question: "question2", answer: "answer"},
-      { question: "question3", answer: "answer"},
-      { question: "question4", answer: "answer"},
-      { question: "question5", answer: "answer"},
-      { question: "question6", answer: "answer"},
-      { question: "question7", answer: "answer"},
-      { question: "question8", answer: "answer"},
-      { question: "question9", answer: "answer"},
-      { question: "question10", answer: "answer"}
-    ]
-  },
-  createGameArea = function (selector) {
-    var doc = window.document,
-        gameArea = doc.createElement("div"),
-        startButton = doc.createElement("button"),
-        timer = document.createElement("span");
-
-    gameArea.setAttribute("class", "game-area");
-    startButton.innerText = "Start";
-    startButton.setAttribute("id", "game-start-button");
-    startButton.onclick = window.Game.start;
-    timer.setAttribute("id", "timer");
-    gameArea.appendChild(startButton);
-    gameArea.appendChild(timer);
-    doc.querySelector(selector).appendChild(gameArea);
-  },
+  var gameData = {},
   disableStartButton = function () {
-    window.document.querySelector("#game-start-button").disabled = true;
-  };
+    $("#game-start-button").prop("disabled", true);
+  },
+  reset = function () {
+    nextQuestionIndex = 0;
+    $("#game-start-button").prop("disabled", false);
+    $(".answer-input").prop("disabled", true);
+    $(".answer-input").val("");
+    $(".question").text("");
+  },
+  answered = function (e) {
+    var code = (e.keyCode ? e.keyCode : e.which),
+        answer;
+    if (code == 13) { //Enter keycode                        
+      e.preventDefault();
+      answer = $(".answer-input").val();
+      renderFeedback(checkAnswer(answer));
+      displayNextChallenge();
+    }
+  },
+  renderFeedback = function (isCorrect) {
+    var fDiv = $("<div>").attr("class", isCorrect? "correct-answer-feedback": "incorrect-answer-feedback");
+    $(".game-feedback").append(fDiv);
+  },
+  checkAnswer = function (answer) {
+    return currentAnswer.indexOf(answer) >= 0;
+  },
+  getNextQuestion = function () {
+    return gameData.challenges[nextQuestionIndex];
+  },
+  nextQuestionIndex = 0,
+  currentQuestion,
+  currentAnswer,
+  displayNextChallenge = function () {
+    var nextQuestion = getNextQuestion();
+    $(".question").text(nextQuestion.question);
+    $(".answer-input").val("");
+    currentQuestion = nextQuestion.question;
+    currentAnswer = nextQuestion.answer;
+    nextQuestionIndex += 1;
+  },
+  removeFeedbacks = function () {
+    var feedbackDiv = $(".game-feedback"),
+        feedbacks = feedbackDiv.find("div");
 
-  window.Game = {
-    init: function (selector) {
-      createGameArea(selector);
-    },
-    start: function () {
-      window.CountDownTimer.start("#timer", 10, function () {alert("time over"); });
+    if (feedbacks.length > 0) {
+      for (var i=0; i < feedbacks.length; i++) {
+        feedbackDiv.removeChild(feedbacks[i]);
+      }
     }
   };
-})(window);
+  window.Game = {
+    init: function () {
+      reset();
+      $(".answer-input").on("keypress", answered);
+      $.ajax("gamedata.json")
+       .done(function (data) { gameData = data;});
+    },
+    start: function () {
+      window.CountDownTimer.start("#timer", 20, reset);
+      $(".answer-input").prop("disabled", false);
+      disableStartButton();
+      removeFeedbacks();
+      displayNextChallenge();
+    }
+  };
+})(window, jQuery);
